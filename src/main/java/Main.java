2eee0f14313dfde4327c548a55073d8f35651d42
DenisBonda <bond250274>
2019-05-16 20:03:13 +0300

    by Den1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,22 +3,16 @@ public class Main {
 	public static void main(String[] args) {
 
 			String path = "C:\\Users\\Автоматика3\\IdeaProjects\\DZpasalgorithms\\Comtrades\\";
-			//String path = "/Users/alexbol96/IdeaProjects/DZpasalgorithms/Comtrades/";
+//			String path = "/Users/alexbol96/IdeaProjects/DZpasalgorithms/Comtrades/";
 //			String path = "C:\\Users\\alexbol96\\IdeaProjects\\DZpasalgorithms\\Comtrades\\";
-			String file = "KZ4";
+			String file = "KZ7";
 			DataComtrade cd = new DataComtrade(path, file);
-//		cd.setChart(ch);
-//		DZChart.create();
-//		Charts.createAnalogChart("Z",0);
-//		Charts.addSeries("Zab",0,0);
 
-//		Charts.createAnalogChart("TRIP ",1);
-//		Charts.addSeries("TripAB",1,0);
 		Charts.createAnalogChart("3I0",0);
 		Charts.addSeries("3I0",0,0);
 		Charts.createAnalogChart("TRIP",1);
 		Charts.addSeries("Trip",1,0);
-//		cd.setChart(ch);
+
 		TZNPrele tznPrele = new TZNPrele();
 		cd.setTznPrele(tznPrele);
 		cd.run();
--- src/main/java/TZNPrele.java
+++ src/main/java/TZNPrele.java
@@ -1,7 +1,5 @@
 public class TZNPrele{
     //Ustavki
-    private double u0ust = 100;
-    private double i0Nom = 100;
     private double i0ust = 800;
     private boolean trip = false;
     private double wMaxSen = 65*Math.PI/180;
@@ -9,12 +7,15 @@ public class TZNPrele{
 
 
     public boolean ReleTZNP(double I0_real, double I0_imag,double U0_real,double U0_imag) {
+
+        //reverse U0 for finding power flow direction
         U0_imag = - U0_imag;
         U0_real = - U0_real;
 
-        double U0 = Math.sqrt(Math.pow(U0_imag, 2)+Math.pow(U0_real, 2));
+        //Finding mod I0
         double Ideistv = Math.sqrt(Math.pow(I0_imag, 2)+Math.pow(I0_real, 2));
 
+        //Finding and correction U0 and IO angle
         double wI = Math.atan(I0_imag/I0_real);
         double wU = Math.atan(U0_imag/U0_real);
 
@@ -30,14 +31,12 @@ public class TZNPrele{
         if (wU<0){
             wU = 2*Math.PI + wU;
         }
-
+        //Finding phase between U0 and I0
         double w0 = wU-wI;
 
-        System.out.println(Ideistv*Math.cos(w0-wMaxSen));
-        System.out.println(wI*180/Math.PI+"////"+wU*180/Math.PI);
-            //System.out.println(Math.atan(I0_imag/I0_real)*180/Math.PI);
 
-            if (Ideistv*Math.cos(w0-wMaxSen)>i0Nom && Ideistv>i0ust) {
+        //Comparision turn on trip and ust
+            if (Ideistv*Math.cos(w0-wMaxSen)>i0ust && Ideistv>i0ust) {
                 trip = true;
             }
 
